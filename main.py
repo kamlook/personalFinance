b935fcc8201ea4498302d6df92b945200c2e94c5
Makkool <38450100+Makkool@users.noreply.github.com>
2020-06-02 14:48:03 -0700

    Create main.py
    Familiarizing with Github

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 14:38:25 2020
+
+@author: Kam Look
+"""
+
+#this will be main script for personalFinance App
+#research and problem statement/clear scope definingmust be done first 
+
+print("Kam and Earn's App")
